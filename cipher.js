dde33fd7efabb714d5fb6947d4579e1733ea14cf
havidtech <76119744+havidtech@users.noreply.github.com>
2021-12-21 15:43:31 +0100

    Create cipher
    Create cipher

---
+++ cipher.js
@@ -0,0 +1,19 @@
+function encode(secret, plainText){
+    let encodedText = '';
+    let originalUnicodeDecimal;
+    let newUnicodeDecimal;
+    for(i = 0; i < plainText.length; i++){
+        originalUnicodeDecimal = plainText.charCodeAt(i);
+        newUnicodeDecimal = originalUnicodeDecimal * secret;
+        encodedText += String.fromCharCode(newUnicodeDecimal)
+    }
+}
+
+function decode(secret, encodedText){
+    let decodedText = '';
+    let originalUnicodeDecimal;
+    for(i = 0; i < encodedText.length; i++){
+        originalUnicodeDecimal = encodedText.charCodeAt(i) / secret;
+        decodedText += String.fromCharCode(originalUnicodeDecimal)
+    }
+}
